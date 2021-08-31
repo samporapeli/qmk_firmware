3a49703d2e40fe69fd1e23b8012a5fb08630717e
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-08-31 21:58:27 +0300

    Add mouse keys

--- keyboards/lily58/keymaps/samporapeli/rules.mk
+++ keyboards/lily58/keymaps/samporapeli/rules.mk
@@ -7,7 +7,7 @@
 BOOTLOADER = qmk-dfu
 
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = no        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
