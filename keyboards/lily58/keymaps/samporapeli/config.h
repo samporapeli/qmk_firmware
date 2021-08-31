5cb826971b1b13993675817f3bb4e30ab5cfb4a2
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2021-08-31 21:58:27 +0300

    Add mouse keys

--- keyboards/lily58/keymaps/samporapeli/config.h
+++ keyboards/lily58/keymaps/samporapeli/config.h
@@ -44,3 +44,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_SLEEP
 */
+
+// mouse keys
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_MOVE_DELTA 4
+#define MOUSEKEY_TIME_TO_MAX 80
+#define MOUSEKEY_MAX_SPEED 16
